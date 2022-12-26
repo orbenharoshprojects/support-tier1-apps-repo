ad05bfe6dcf59f55e39519acd15d7d176799bdb0
orbenharoshprojects <orbenharoshprojects@gmail.com>
2022-12-26 17:35:54 +0200

    Update pu-script.rb

--- pu-script.rb
+++ pu-script.rb
@@ -8,22 +8,13 @@ class SupportTier1AppsRepo < Formula
   sha256 "719169fed451fe9cce434ae335783f3946c3f7bf947046166bcd829366523dc3"
   license ""
 
-  depends_on "pu-script"
+ #depends_on "pu-script"
 
   def install
     bin.install "pu-script"
   end
 
   test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! For Homebrew/homebrew-core
-    # this will need to be a test that verifies the functionality of the
-    # software. Run the test with `brew test support-tier1-apps-repo`. Options passed
-    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system "#{bin}/program", "do", "something"`.
-    system "false"
+    system "#{bin}/pu-script", "--help"
   end
 end
