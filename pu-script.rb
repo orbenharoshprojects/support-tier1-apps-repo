a8d17203850dd74fb10f666b43c170f32631ef28
orbenharoshprojects <orbenharoshprojects@gmail.com>
2022-12-26 17:39:00 +0200

    Update pu-script.rb

--- pu-script.rb
+++ pu-script.rb
@@ -3,7 +3,7 @@
 # PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class SupportTier1AppsRepo < Formula
   desc "The needed "day-to-day" apps for a tier 1 support engineer  "
-  homepage "https://github.com/orbenharoshprojects/homebrew-support-tier1-apps-repo"
+  homepage "https://github.com/orbenharoshprojects/homebrew-support-tier1-apps-repo.git"
   url "https://github.com/orbenharoshprojects/support-tier1-apps-repo/archive/refs/tags/v0.1.0.tar.gz"
   sha256 "719169fed451fe9cce434ae335783f3946c3f7bf947046166bcd829366523dc3"
   license ""
