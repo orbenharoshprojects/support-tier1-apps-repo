2c3d97a69815d06979f054033abe66a402abdd9c
orbenharoshprojects <orbenharoshprojects@gmail.com>
2022-12-26 22:35:31 +0200

    .

--- apps-script.rb
+++ apps-script.rb
@@ -1,6 +1,8 @@
 class TmuxCssh < Formula
+class Apps < Formula
   desc "apps-script that will install all u need buddy"
   homepage "https://github.com/knakayama/apps-script"
+  homepage "https://github.com/orbenharoshprojects/apps-script"
   url "https://github.com/orbenharoshprojects/homebrew-support-tier1-apps-repo/archive/refs/tags/v0.1.0.tar.gz"
   sha256 "ce761f21fa0fe7050f408e687f9a4964acf95736c85678e3cc27cee88bd6ceed"
   version "1.0"
