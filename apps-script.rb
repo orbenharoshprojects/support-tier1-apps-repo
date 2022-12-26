63b05cf44eacc5d72c97b2827458fe6eedeacdb7
orbenharoshprojects <orbenharoshprojects@gmail.com>
2022-12-26 21:54:37 +0200

    new script was added for test purposes

---
+++ apps-script.rb
@@ -0,0 +1,33 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class SupportTier1AppsRepo < Formula
+  desc ""
+  homepage ""
+  url "https://github.com/orbenharoshprojects/homebrew-support-tier1-apps-repo/archive/refs/tags/v0.1.0.tar.gz"
+  sha256 "6b78504e8b20159416675543060e799e67717a78968cc6542ea88f9e1d57c670"
+  license ""
+
+  # depends_on "cmake" => :build
+
+  def install
+    # ENV.deparallelize  # if your formula fails when building in parallel
+    # Remove unrecognized options if warned by configure
+    # https://rubydoc.brew.sh/Formula.html#std_configure_args-instance_method
+    system "./configure", *std_configure_args, "--disable-silent-rules"
+    # system "cmake", "-S", ".", "-B", "build", *std_cmake_args
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test support-tier1-apps-repo`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "false"
+  end
+end
