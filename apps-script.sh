b60883b2c955886479a5c6ed74edc4ca5954d6b7
orbenharoshprojects <orbenharoshprojects@gmail.com>
2022-12-27 16:22:50 +0200

    Update apps-script.sh

--- apps-script.sh
+++ apps-script.sh
@@ -29,7 +29,7 @@ echo '
 # Install if we don't have it
 if test ! $(which brew); then
   echo "Installing homebrew..."
-  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)" && yes | brew <...>
+  yes | /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)" 
 fi
 
 # Update homebrew recipes
