3ee5878c0ffa626958503a247c8b2b6dfc88063f
orbenharoshprojects <orbenharoshprojects@gmail.com>
2022-12-27 11:06:50 +0200

    Update apps-script.rb

--- apps-script.rb
+++ apps-script.rb
@@ -1,7 +1,7 @@
 class Apps < Formula
   desc "This script that will install all u need buddy"
   homepage "https://github.com/orbenharoshprojects/apps-script"
-  url "https://github.com/orbenharoshprojects/homebrew-support-tier1-apps-repo/archive/refs/tags/v0.1.0.tar.gz"
+  url "https://github.com/orbenharoshprojects/homebrew-support-tier1-apps-repo/archive/refs/tags/v0.2.0.tar.gz"
   sha256 "ce761f21fa0fe7050f408e687f9a4964acf95736c85678e3cc27cee88bd6ceed"
   version "1.0"
 
